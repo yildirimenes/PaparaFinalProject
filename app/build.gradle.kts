755fe84eb2f556646ffb64ed4c319a136f285f6b
yildirimenes <104741427+yildirimenes@users.noreply.github.com>
2024-07-31 18:03:45 +0300

    update retrofit room and hilt version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     kotlin("plugin.serialization") version "1.8.20"
-    id("kotlin-kapt")
+    id("com.google.devtools.ksp")
     id("dagger.hilt.android.plugin")
     id("com.google.dagger.hilt.android")
     id("com.google.gms.google-services")
@@ -84,16 +84,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    /*
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-    */
-
     implementation(libs.ktor.client.cio.jvm)
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.json)
@@ -101,45 +91,41 @@ dependencies {
     implementation (libs.accompanist.pager)
 
     //Navigation
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation(libs.androidx.navigation.compose)
 
     //Coil
-    implementation("io.coil-kt:coil-compose:2.4.0")
-    implementation ("com.github.skydoves:landscapist-coil:2.3.1")
+    implementation(libs.coil.compose)
+    implementation (libs.landscapist.coil)
 
     //SplashScreen
-    implementation ("com.airbnb.android:lottie-compose:6.3.0")
-    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation(libs.androidx.core.splashscreen)
+    implementation (libs.lottie.compose)
 
     //Google Fonts
-    implementation("androidx.compose.ui:ui-text-google-fonts:1.6.5")
+    implementation(libs.androidx.ui.text.google.fonts)
 
     //Notification
-    implementation ("androidx.work:work-runtime-ktx:2.7.1")
+    implementation (libs.androidx.work.runtime.ktx)
 
     //Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
 
     //Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation(libs.kotlinx.coroutines.android)
 
     // Dagger Hilt
     implementation(libs.hilt.android)
-    kapt(libs.dagger.hilt.android.compiler)
-    kapt(libs.androidx.hilt.compiler)
+    ksp(libs.dagger.hilt.android.compiler)
+    ksp(libs.androidx.hilt.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
 
     //Room
-    implementation("androidx.room:room-runtime:2.5.2")
-    implementation ("androidx.room:room-ktx:2.5.2")
-    kapt("androidx.room:room-compiler:2.5.2")
+    implementation(libs.androidx.room.runtime)
+    implementation (libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
 
+    //Firebase
     implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
     implementation("com.google.firebase:firebase-analytics")
-}
-
-kapt {
-    correctErrorTypes = true
-    useBuildCache = true
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ buildscript {
 plugins {
     id("com.android.application") version "8.5.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false
     id("com.google.dagger.hilt.android") version "2.49" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
